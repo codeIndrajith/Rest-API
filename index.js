6352f3d9fb94335e3fd2d50a9673d78557a502d5
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-09-28 23:46:01 +0530

    check the get request
    install node and express then create app in express.
    add basic command in check the get request is working.

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRoute from './routes/users.js';
+
+const app = express()
+const PORT = 5000;
+
+// the common way (json format) to exchange data request and response in rest api
+app.use(bodyParser.json())
+
+app.listen(PORT , () => console.log(`app listen on http://localhost:${PORT}`))
+
+app.get('/' , (req , res) => {
+    res.send("Hello home page")
+})
+
+app.get('/users' , userRoute)
+
+
+
