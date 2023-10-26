d5b84460a71f51f37b9fb805ea753f67145cae62
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-10-26 18:42:54 +0530

    GET request
    create users.js file and handle the GET requests.

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import userRoute from './routes/users.js';
 
+import userRoutes from './routes/users.js'
+
 const app = express()
 const PORT = 5000;
 
 // the common way (json format) to exchange data request and response in rest api
 app.use(bodyParser.json())
+app.use('/users' , userRoutes)
 
 app.listen(PORT , () => console.log(`app listen on http://localhost:${PORT}`))
 
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,46 @@
-
-import express from 'express'
+import express from 'express';
 
 const router = express.Router();
 
-router.get('/users' , (req , res) => {
-    res.send("hello users file")
-})
+const users = [
+  {
+    firstName: 'Emily',
+    lastName: 'Brown',
+    age: 26,
+  },
+  {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 30,
+  },
+  {
+    firstName: 'Sarah',
+    lastName: 'Johnson',
+    age: 28,
+  },
+  {
+    firstName: 'Michael',
+    lastName: 'Williams',
+    age: 35,
+  },
+  {
+    firstName: 'David',
+    lastName: 'Wilson',
+    age: 32,
+  },
+  {
+    firstName: 'Sophia',
+    lastName: 'Miller',
+    age: 29,
+  },
+];
+
+// all routes in here are starting with /users
+router.get('/', (req, res) => {
+  console.log(users);
+  res.send(users);
+});
+
+router.post('/', (req, res) => {});
 
 export default router;
